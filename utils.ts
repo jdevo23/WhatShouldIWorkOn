fcc8248331bb4ac7563e59699d52c0fd3c8af852
Jonny Deveson <jrdeveson@hotmail.com>
2023-11-19 21:53:34 +0000

    delete file

--- utils.ts
+++
@@ -1,6 +0,0 @@
-export default function createButton(text: string, classes: string[]): HTMLButtonElement {
-	const button = document.createElement("button");
-	button.textContent = text;
-	button.classList.add(...classes);
-	return button;
-}
